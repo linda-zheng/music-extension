df5e005feb4df263cdcb9d8c6f7ef34cf12c2161
Hannah Li <hannahli2010@gmail.com>
2020-11-21 16:11:31 -0500

    Improve UI

--- background.js
+++ background.js
@@ -15,7 +15,7 @@ chrome.runtime.onInstalled.addListener(function() {
 // don't need if declaratively injecting content script
 chrome.runtime.onMessage.addListener(
   function(message, sender, callback) {
-    // alert(`background: ${JSON.stringify(message)}, ${JSON.stringify(sender)}, ${JSON.stringify(callback)}`);
+    console.log(`background: ${JSON.stringify(message)}, ${JSON.stringify(sender)}, ${JSON.stringify(callback)}`);
     // sender example: {"id":"hligmjmffggjjpbnilddcnmmpecgpglc","url":"chrome-extension://hligmjmffggjjpbnilddcnmmpecgpglc/index.html","origin":"chrome-extension://hligmjmffggjjpbnilddcnmmpecgpglc"}
     if (message.event == 'parsePage'){
       chrome.tabs.executeScript({
