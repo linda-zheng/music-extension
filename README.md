d144821d7bb9463a80bbbe49eb8d38ec40499fc0
selinali2010 <selinali2010@gmail.com>
2020-11-21 11:54:52 -0500

    Update README and add more content to the popup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # music-extension
+
+### Development
+1. Clone this repo
+2. Navigate to `chrome://extensions/`
+3. Enable `Developer Mode`
+4. Click on `Load unpacked` and select the project folder
+5. You can now click on the extension to view it
--- background.js
+++ background.js
@@ -5,9 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
+        new chrome.declarativeContent.PageStateMatcher({})
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
